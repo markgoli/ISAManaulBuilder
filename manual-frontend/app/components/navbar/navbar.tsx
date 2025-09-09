62e1467eb11e286ac06fd1bd8da05046aa55072a
markgoli <golimark3@gmail.com>
2025-09-09 14:21:19 +0300

    ISA Manual builder system.

---
+++ manual-frontend/app/components/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <nav className="w-full fixed left-0 top-0 border-b p-6 bg-white z-10">
+            this is navbar help text.
+        </nav>
+    )
+}
+
+
+export default Navbar;
